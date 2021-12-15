92aa3446751cd13a206516b3b95fc3d910646738
James Yaro <onuhjamesyaro@gmail.com>
2021-12-15 11:05:31 +0100

    Changes folder name Images to images

--- src/Components/Back/Data/Data.js
+++ src/Components/Back/Data/Data.js
@@ -4,111 +4,111 @@ const data = {
             id: "1",
             name: "Liverpool FC - Home",
             price: 4999,
-            image:'/Images/liverpool2.jpg'
+            image:'/images/liverpool2.jpg'
         },
 
         {
             id: "2",
             name: "Liverpool FC - Away",
             price: 4999,
-            image:'/Images/liverpool.jpg'
+            image:'/images/liverpool.jpg'
         },
         {
             id: "3",
             name: "Manchester City FC - Home",
             price: 4999,
-            image:'/Images/city2.jpg'
+            image:'/images/city2.jpg'
         },
         {
             id: "4",
             name: "Manchester City FC - Away",
             price: 4999,
-            image:'/Images/city.jpg'
+            image:'/images/city.jpg'
         },
         {
             id: "5",
             name: "FC Barcelona - Home",
             price: 4999,
-            image:'/Images/fcb.jpg'
+            image:'/images/fcb.jpg'
         },
         {
             id: "6",
             name: "Manchester United FC - Home",
             price: 4999,
-            image:'./Images/Manutd.png'
+            image:'/images/Manutd.png'
         },
         {
             id: "7",
             name: "Manchester United FC - Away",
             price: 4999,
-            image:'./Images/manutd2.jpg'
+            image:'/images/manutd2.jpg'
         },
         {
             id: "8",
             name: "Real Madrid - Home",
             price: 4999,
-            image:'./Images/real.jpg'
+            image:'/images/real.jpg'
         },
         {
             id: "9",
             name: "Atletico Madrid FC - Home",
             price: 4999,
-            image:'./Images/atletico.jpg'
+            image:'/images/atletico.jpg'
         },
         {
             id: "10",
             name: "Atletico Madrid FC - Away",
             price: 4999,
-            image:'./Images/atletico2.jpg'
+            image:'/images/atletico2.jpg'
         },
 
         {
             id: "11",
             name: "PSG - Home",
             price: 4999,
-            image:'./Images/psg.jpg'
+            image:'/images/psg.jpg'
         },
         {
             id: "12",
             name: "PSG - Away",
             price: 4999,
-            image:'./Images/psg2.jpg'
+            image:'/images/psg2.jpg'
         },
         {
             id: "13",
             name: "Chelesa FC - Home",
             price: 4999,
-            image:'./Images/chelsea3.jpg'
+            image:'/images/chelsea3.jpg'
         },
         {
             id: "14",
             name: "Chelsea FC - Away",
             price: 4999,
-            image:'./Images/chelsea2.jpg'
+            image:'/images/chelsea2.jpg'
         },
         {
             id: "15",
             name: "Ajax FC - Away",
             price: 4999,
-            image:'./Images/ajax2.png'
+            image:'/images/ajax2.png'
         },
         {
             id: "16",
             name: "Arsenal FC - Home",
             price: 4999,
-            image:'./Images/arsenal.png'
+            image:'/images/arsenal.png'
         },
         {
             id: "17",
             name: "Tottenham Hotspur FC - Home",
             price: 4999,
-            image:'./Images/tottenham.jpg'
+            image:'/images/tottenham.jpg'
         },
         {
             id: "18",
             name: "Tottenham Hotspur FC - Away",
             price: 4999,
-            image:'./Images/tottenham2.jpg'
+            image:'/images/tottenham2.jpg'
         }
     ]
 }
