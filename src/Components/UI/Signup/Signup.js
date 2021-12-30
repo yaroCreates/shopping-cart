83823e7c1d873db1c449cbf3cba3e878b1621776
James Yaro <onuhjamesyaro@gmail.com>
2021-12-30 14:57:30 +0100

    Jersey store logo

--- src/Components/UI/Signup/Signup.js
+++ src/Components/UI/Signup/Signup.js
@@ -1,10 +1,15 @@
 import React from "react"
+import Jerseystore from "../../../images/jerserystore.svg"
 
 
 function Signup(){
     return(
         <div className='signup'>
-            <div className="left"></div>
+            <div className="left">
+                <div className="heading">
+                    <img src={Jerseystore} alt="Jerseystore"/>
+                </div>
+            </div>
             <div className="right"></div>
         </div>
     )
